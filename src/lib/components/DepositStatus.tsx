839b13d79e1f471511cda92ed3490af94e6c9b34
Misty-clouds <adenijitaoheed26@gmail.com>
2025-02-08 14:54:03 +0100

    button-fixed

--- src/lib/components/DepositStatus.tsx
+++ src/lib/components/DepositStatus.tsx
@@ -219,7 +219,7 @@ export default function DepositStatus() {
                     <button
                       onClick={() => verifyDeposit(deposit)}
                       disabled={verifyingDepositId===deposit.id}
-                      className="w-full mt-4 bg-green-600 hover:bg-green-700 text-green-500 font-bold py-2 rounded-xl flex items-center justify-center space-x-2  "
+                      className="w-full mt-4 bg-blue-600 hover:bg-green-700  font-bold py-2 rounded-xl flex items-center justify-center space-x-2  "
                     >
                       <AlertCircle className="w-5 h-5" />
                       <span>{t('verifyTransaction')}</span>
